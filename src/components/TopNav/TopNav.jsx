6a7e50c61a9b4fc50e169ccd1d8a7763265935b6
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2022-07-25 22:50:44 -0400

    trying something

--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -1,16 +1,37 @@
 import './TopNav.css';
 import LeftLink from '../LeftLink/LeftLink.jsx'
 import RightNav from '../RightNav/RightNav.jsx'
+import { Link } from 'react-router-dom';
 
 
 export default function TopNav() {
-   return (
-       <div className="Nav">
-         <LeftLink />
-         <RightNav />
-        
-       </div>
-   )
+    return (
+        <div className="Nav">
+            <Link className="nav-left-link" to="/portfolio">TATYANA KARLEN - FULL STACK DEVELOPER</Link>
+            <button className="hamburger">
+                {/* icon from heroicons.com */}
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="white"
+                >
+                    <path
+                        fillRule="evenodd"
+                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                        clipRule="evenodd"
+                    />
+                </svg>
+            </button>
+            <div
+                className="navigation-menu">
+                <ul>
+                    <li><Link className="link" to="/portfolio">PORTFOLIO</Link></li>
+                    <li><Link className="link" to="/resume">RESUME</Link></li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 
