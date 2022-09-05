5750d302faafca61189fbc12934e755c88cba106
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2022-09-05 19:18:26 -0400

    added something

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
       </div>
       <br />
       <article class="article">
-        Hello! I'm a full-stack developer with a passion for UI/front-end. I'm experienced in both front and back-end development and up-to-date with the industry’s most
+        Hello! I'm a web developer with a passion for UI/front-end. I'm experienced in both front and back-end development and up-to-date with the industry’s most
         in-demand technologies. My background in fashion marketing gives me a
         creative edge. When I'm not coding I'm playing/teaching piano, thrifting or watching bad movies. 
       </article>
