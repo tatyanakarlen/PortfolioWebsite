edba0462c9afd67c897eed10a40422bcd264536f
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2022-09-16 12:09:03 -0400

    fixed svg

--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -27,6 +27,7 @@ export default function TopNav() {
       >
         {/* icon from heroicons.com */}
         <svg
+          style={{backgroundColor: 'unset'}}
           xmlns="http://www.w3.org/2000/svg"
           className="h-5 w-5"
           viewBox="0 0 20 20"
