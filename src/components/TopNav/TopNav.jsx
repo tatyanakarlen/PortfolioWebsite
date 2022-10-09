e2624dd941098a60fa4038a4600a47eac3de2bb3
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2022-10-09 17:02:20 -0400

    temp fixes

--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -3,7 +3,7 @@ import './TopNav.css';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
-import logo from '../../components/Images/TKcropped.png'
+import logo from '../../components/Images/Taty.png'
 
 export default function TopNav() {
   const [isNavExpanded, setIsNavExpanded] = useState(false);
@@ -23,8 +23,9 @@ export default function TopNav() {
     <div className="Nav">
       <div id="name-link">
         <a onClick={linkNavStateTrigger} className="nav-left-link" href={aboutScrollTo}>
-          <img src={logo} style={{width: '60px', marginTop: '0.5rem'}}/>
+          <img src={logo} style={{width: '55px', marginTop: '0.5rem'}}/>
         </a>
+        
       </div>
 
       <button
