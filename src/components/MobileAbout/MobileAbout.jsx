9fa9a27d40a778efa0681434ef9c165bc78f9a23
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2022-10-10 23:05:32 -0400

    calling it a night

--- src/components/MobileAbout/MobileAbout.jsx
+++ src/components/MobileAbout/MobileAbout.jsx
@@ -7,7 +7,7 @@ const MobileAbout = () => {
       <h1>&lt;ABOUT ME&gt;</h1>
       <p>
         My name is <span class="bold-span">Tatyana</span> and I'm a <span class="bold-span">web developer</span> with a background in fashion and classical music. When I'm not coding, I'm playing or teaching piano. My skills and past
-        projects can be found below. 
+        projects can be found below.
         <br/><br/><span style={{fontWeight:'bold'}}>Looking to collaborate or hire me? I'd love
         to hear from you!</span>
       </p>
