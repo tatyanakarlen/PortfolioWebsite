0c140d40c04589170c559219db5777705139be61
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2023-01-17 13:54:49 -0500

    changed some bio

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,17 +7,18 @@ const About = () => {
     <div class="about-me">
       {/* <h3 class="h3-computer">Diverse creative background</h3> */}
       <p class="paragraph">
-        Welcome and thanks for stopping by! Based in
+        Welcome and thanks for stopping by! 
+        Based in
         Toronto, Canada, I have a <span>diverse creative background</span> in
         fashion, piano performance and music pedagogy.
         <br />
-        <br />I graduated from <span>General Assembly's</span>{' '}
+        <br />Always curious about the web, I enrolled in <span>General Assembly's</span>{' '}
         <span class="neon-green-text">Software Engineering Immersive</span>{' '}
         program where I learned full-stack development in a fast-paced
         environment.
-        {/* <br />
         <br />
-       When I'm not coding, I'm playing or teaching piano. I'm inspired by cult movies, the 80's and a DYI aesthetic. */}
+        <br />
+       When I'm not coding, I'm playing or teaching piano. I'm inspired by cult movies, the 80's and much more.
       </p>
     </div>
   );
