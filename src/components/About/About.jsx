e55945ded4c2c2f4680bc74165f1c39329bae396
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2023-01-17 11:46:33 -0500

    cleaned up code

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,14 +7,14 @@ const About = () => {
     <div class="about-me">
       {/* <h3 class="h3-computer">Diverse creative background</h3> */}
       <p class="paragraph">
-        Welcome and thanks for stopping by! I'm a{' '}
-         junior developer based in Toronto, Canada.
-        I have a <span>diverse creative background</span> in fashion, piano performance and music pedagogy.
+        Welcome and thanks for stopping by! I'm a junior developer based in
+        Toronto, Canada. I have a <span>diverse creative background</span> in
+        fashion, piano performance and music pedagogy.
         <br />
-        <br />
-        I graduated from <span>General Assembly's</span>{' '}
-        <span class="neon-green-text">Software Engineering Immersive</span> program{' '}
-        where I learned full-stack development in a fast-paced environment. 
+        <br />I graduated from <span>General Assembly's</span>{' '}
+        <span class="neon-green-text">Software Engineering Immersive</span>{' '}
+        program where I learned full-stack development in a fast-paced
+        environment.
         {/* <br />
         <br />
        When I'm not coding, I'm playing or teaching piano. I'm inspired by cult movies, the 80's and a DYI aesthetic. */}
