6a829879f07ad4a7491a2b1d59c32ed571e154ec
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2023-09-13 15:22:29 -0400

    fixed footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div style={{ padding: '3rem', textAlign: 'center', fontSize: '1.8rem', fontWeight: '800'}}>
+    <div style={{ marginTop: '12rem', fontSize: '1.8rem', fontWeight: '800'}}>
       &#169; 2023 Tatyana Karlen
     </div>
   )
