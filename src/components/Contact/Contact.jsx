010b7bd597b65bf398016d62eac37393921f0d68
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2023-09-13 15:18:58 -0400

    fixed contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import { MdOutlineMailOutline } from 'react-icons/md';
 import './Contact.css';
-
 const Contact = () => {
   return (
-    <div class="contact" id="contact">
-      <h1 class="section-heading">Contact</h1>
-      <p>Looking to collaborate or hire me? I'd love to hear from you!</p>
-      <div class="email">
-        <h3>E-mail me</h3>
+    <div className="section-container contact" id="contact">
+      {' '}
+      <h2 className="h2-heading">Contact</h2>{' '}
+      <p>Looking to collaborate or hire me? I'd love to hear from you!</p>{' '}
+      <div className="email">
+        {' '}
+        <h2>E-mail me</h2>{' '}
         <a
-          class="social-links"
+          className="social-links"
           href="mailto:tatyanakarlen@gmail.com"
           target="_blank"
           aria-label="Mail"
           rel="noopener noreferrer"
         >
-          <MdOutlineMailOutline />
-        </a>
-      </div>
+          {' '}
+          <MdOutlineMailOutline />{' '}
+        </a>{' '}
+      </div>{' '}
     </div>
   );
 };
-
 export default Contact;
